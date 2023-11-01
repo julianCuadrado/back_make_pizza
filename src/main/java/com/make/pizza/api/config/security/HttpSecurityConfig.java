9eaed618fab0f8d421695e8c99985b8ddd08f6e0
jcuadrado <julian.cuadrado@intempo.co>
2023-11-01 00:23:10 -0500

    update

--- src/main/java/com/make/pizza/api/config/security/HttpSecurityConfig.java
+++ src/main/java/com/make/pizza/api/config/security/HttpSecurityConfig.java
@@ -61,10 +61,10 @@ public class HttpSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8100"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
+//        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
